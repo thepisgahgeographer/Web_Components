b046980bdcf61bf3e1ecdf7f1f319d3063bbd1ee
YP <potawsky.yuri@gmail.com>
2022-02-15 20:09:13 -0500

    Slot

---
+++ Form_Entry/app.js
@@ -0,0 +1,47 @@
+class MyComponent extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `<h1>Hello world</h1>`;
+  }
+}
+
+customElements.define('my-component', MyComponent);
+
+
+
+
+const shadowRoot = document.getElementById('example').attachShadow({
+  mode: 'open'
+});
+shadowRoot.innerHTML = `<style>
+button {
+  background: tomato;
+  color: white;
+}
+</style>
+<button id="button"><slot></slot> tomato cheese</button>`;
+
+
+const fragment = document.getElementById('book-template');
+const books = [{
+    title: 'The Great Gatsby',
+    author: 'F. Scott Fitzgerald'
+  },
+  {
+    title: 'A Farewell to Arms',
+    author: 'Ernest Hemingway'
+  },
+  {
+    title: 'Catch 22',
+    author: 'Joseph Heller'
+  }
+];
+
+books.forEach(book => {
+  // Create an instance of the template content
+  const instance = document.importNode(fragment.content, true);
+  // Add relevant content to the template
+  instance.querySelector('.title').innerHTML = book.title;
+  instance.querySelector('.author').innerHTML = book.author;
+  // Append the instance ot the DOM
+  document.getElementById('books').appendChild(instance);
+});
