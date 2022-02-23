f7ffe82474765cb2e3804a1abe77d75de5dd9d0d
YP <potawsky.yuri@gmail.com>
2022-02-23 12:03:29 -0500

    HTMLTemplates

--- Form_Entry/app.js
+++ Form_Entry/app.js
@@ -1,84 +1,56 @@
-// class MyComponent extends HTMLElement {
-//   connectedCallback() {
-//     this.innerHTML = `<h1>Hello world</h1>`;
-//   }
-// }
-//
-// customElements.define('my-component', MyComponent);
-//
-//
-//
-//
-// const shadowRoot = document.getElementById('example').attachShadow({
-//   mode: 'open'
-// });
-// shadowRoot.innerHTML = `<style>
-// button {
-//   background: tomato;
-//   color: white;
-// }
-// </style>
-// <button id="button"><slot></slot> tomato cheese</button>`;
-//
-//
-// const fragment = document.getElementById('book-template');
-// const books = [{
-//     title: 'The Great Gatsby',
-//     author: 'F. Scott Fitzgerald'
-//   },
-//   {
-//     title: 'A Farewell to Arms',
-//     author: 'Ernest Hemingway'
-//   },
-//   {
-//     title: 'Catch 22',
-//     author: 'Joseph Heller'
-//   }
-// ];
-//
-// books.forEach(book => {
-//   // Create an instance of the template content
-//   const instance = document.importNode(fragment.content, true);
-//   // Add relevant content to the template
-//   instance.querySelector('.title').innerHTML = book.title;
-//   instance.querySelector('.author').innerHTML = book.author;
-//   // Append the instance ot the DOM
-//   document.getElementById('books').appendChild(instance);
-// });
-//
-//
-// const template = document.getElementById('template');
-//
-// document.body.appendChild(
-//   document.importNode(template.content, true)
-// );
+'use strict';
 
-class OneDialog extends HTMLElement {
-  connectedCallback() {
-    const template = document.getElementById('one-dialog');
-    const node = document.importNode(template.content, true);
-    this.appendChild(node);
+const template = document.querySelector('template');
+const node = document.importNode(template.content, true);
+document.body.appendChild(node);
+
+
+
+
+const books = [{
+    title: 'The Great Gatsby',
+    author: 'F. Scott Fitzgerald'
+  },
+  {
+    title: 'A Farewell to Arms',
+    author: 'Ernest Hemingway'
+  },
+  {
+    title: 'Catch 22',
+    author: 'Joseph Heller'
   }
+];
+
+function appendBooks(templateId) {
+  const booksList = document.getElementById('books');
+  const fragment = document.getElementById(templateId);
+
+  // Clear out the content from the ul
+  booksList.innerHTML = '';
+
+  // Loop over the books and modify the given template
+  books.forEach(book => {
+    // Create an instance of the template content
+    const instance = document.importNode(fragment.content, true);
+    // Add relevant content to the template
+    instance.querySelector('.title').innerHTML = book.title;
+    instance.querySelector('.author').innerHTML = book.author;
+    // Append the instance ot the DOM
+    booksList.appendChild(instance);
+  });
 }
 
 
-let ary = ["Shinny Creek", "Jacobs Fork", "Little River"];
-// console.log(ary);
+document.getElementById('templates').addEventListener('change', (event) => appendBooks(event.target.value));
 
-ary.forEach(element => console.log(element));
-
-let ary1 = [2,4,6];
-let test1 = ary1.map(X => X*2);
-console.log(test1);
-
-const test2 = ary.filter(word => word.length >= 12);
-console.log(test2);
-
-const test3 = ary.find(val => val.includes("Shi"));
-console.log(test3);
+appendBooks('book-template');
 
 
 
 
 
-//
+
+
+
+
+// //
