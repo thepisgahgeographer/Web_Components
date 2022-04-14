a75d7271663d3ada8e6e29d1249bed6d4c77d98a
YP <potawsky.yuri@gmail.com>
2022-04-14 16:33:42 -0400

    WebComponents

--- Form_Entry/app.js
+++ Form_Entry/app.js
@@ -4,8 +4,82 @@ const template = document.querySelector('template');
 const node = document.importNode(template.content, true);
 document.body.appendChild(node);
 
+// let theNumber = Number(prompt("Pick a number"));
+// if (!Number.isNaN(theNumber)) {
+//   console.log("Your number is the square root of " +
+//               theNumber * theNumber);
+// } else {
+//   console.log("Hey. Why didn't you give me a number?");
+// }
+
+// let result = 1;
+// let counter = 0;
+// while (counter < 10) {
+//   result = result * 2;
+//   counter = counter + 1;
+// }
+// console.log(result);
+
+// for (let line = "#"; line.length < 8; line += "#")
+//   console.log(line);
+
+//   const hummus = function(factor) {
+//     const ingredient = function(amount, unit, name) {
+//       let ingredientAmount = amount * factor;
+//       if (ingredientAmount > 1) {
+//         unit += "s";
+//       }
+//       console.log(`${ingredientAmount} ${unit} ${name}`);
+//     };
+//     ingredient(1, "can", "chickpeas");
+//     ingredient(0.25, "cup", "tahini");
+//     ingredient(0.25, "cup", "lemon juice");
+//     ingredient(1, "clove", "garlic");
+//     ingredient(2, "tablespoon", "olive oil");
+//     ingredient(0.5, "teaspoon", "cumin");
+//   };
+
+function reverse(param){
+  let val = new Array();
+  let mapVal = val.map(x=>param);
+  // let tst = mapVal.reverse();
+  // console.log(tst);
+  console.log(mapVal);
+}
+
+// // reverse();
+
+// function reverseNum(param){
+//   param = param.toString();
+//   return param.split("").reverse().join("");
+// }
+
+// console.log(reverseNum(12345));
+
+// function alpha(param){
+//   //param = param.toString();
+//   return param.split("").sort().join("");
+// }
+
+// console.log(alpha("jasdlfjakdsf"));
+
+// function toUpper(param2){
+//   console.log(param2.toUpperCase());
+// }
+
+// toUpper("the brushy mountains");
+
+// const str = 'flexiple';
+// const str2 = str.charAt(0).toUpperCase() + str.slice(1);
+// console.log(str2);
+let str = "the brushy mountains";
+
+let splt = str.split("");
+console.log(typeof(splt));
 
 
+// let test = [1,2,3,4,5];
+// console.log(test.reverse());
 
 // const books = [{
 //     title: 'The Great Gatsby',
---
+++ wc.js
@@ -0,0 +1,4 @@
+// let select = document.getElementById("tmpl");
+// let newNode = document.importNode(select, true);
+// newNode.innerHTML("Yo");
+// document.body.appendChild(newNode);
